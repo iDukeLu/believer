96e05e64d4baec42c55e972f5b30dcc1b0596048
duke.lu <duke.lu@ideamake.cn>
2021-01-29 12:42:12 +0800

    init 🎉

---
+++ core/env/conf.go
@@ -0,0 +1,43 @@
+package env
+
+import (
+	"github.com/iDukeLu/believer/core/util"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	DEFAULT_CONF_PATH = "/app/conf/app.yml"
+)
+
+type conf struct {
+	Server     `yaml:"server"`
+	Datasource `yaml:"datasource"`
+}
+
+type Server struct {
+	Port        int    `yaml:"port"`
+	ContextPath string `yaml:"port"`
+}
+
+type Datasource struct {
+	Url      string `yaml:"url"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+func load() *conf {
+	//应该是 绝对地址
+	pwd, e := os.Getwd()
+	util.Panic(e)
+
+	yamlFile, e := ioutil.ReadFile(pwd + DEFAULT_CONF_PATH)
+	util.Panic(e)
+
+	c := new(conf)
+	e = yaml.Unmarshal(yamlFile, c)
+	util.Panic(e)
+
+	return c
+}
