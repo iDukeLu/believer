5cea01e53f139a4f172631e899f33625fbd46d25
duke.lu <duke.lu@ideamake.cn>
2021-01-29 18:04:55 +0800

    update conf

---
+++ core/app/app.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"github.com/iDukeLu/believer/core/env"
+)
+
+func Run() {
+	// load configuration
+	conf := env.Load()
+	// init server
+
+	// init database
+}
+
+func initServer(c *Conf) {
+
+}
--- core/env/conf.go
+++ core/env/conf.go
@@ -11,7 +11,8 @@ const (
 	DEFAULT_CONF_PATH = "/app/conf/app.yml"
 )
 
-type conf struct {
+type Conf struct {
+	Profile    string `yaml:"profile"`
 	Server     `yaml:"server"`
 	Datasource `yaml:"datasource"`
 }
@@ -27,17 +28,69 @@ type Datasource struct {
 	Password string `yaml:"password"`
 }
 
-func Load() *conf {
-	//应该是 绝对地址
+// load and parse configuration file
+func Load() *Conf {
+	return doLoad()
+}
+
+func doLoad() *Conf {
+	defaultConf := parse(read(""))
+	if profile := defaultConf.Profile; profile != "" {
+		profileConf := parse(read(profile))
+		return merge(defaultConf, profileConf)
+	}
+	return defaultConf
+}
+
+// merge configuration file
+func merge(defaultConf *Conf, profileConf *Conf) *Conf {
+	mergeConf := new(Conf)
+	mergeConf.Profile = defaultConf.Profile
+	mergeConf.Port = getIntProperty(defaultConf.Port != 0, defaultConf.Port, profileConf.Port)
+	mergeConf.ContextPath = getStringProperty(defaultConf.ContextPath != "", defaultConf.ContextPath, profileConf.ContextPath)
+	mergeConf.Url = getStringProperty(defaultConf.Url != "", defaultConf.Url, profileConf.Url)
+	mergeConf.Username = getStringProperty(defaultConf.Username != "", defaultConf.Username, profileConf.Username)
+	mergeConf.Password = getStringProperty(defaultConf.Password != "", defaultConf.Password, profileConf.Password)
+	return mergeConf
+}
+
+// parse yml []byte to specified struct
+func parse(yml []byte) *Conf {
+	c := new(Conf)
+	e := yaml.Unmarshal(yml, c)
+	util.Panic(e)
+	return c
+}
+
+// read the configuration file of the specified profile
+func read(profile string) []byte {
+	yml, e := ioutil.ReadFile(getConfFilePath(profile))
+	util.Panic(e)
+	return yml
+}
+
+// get the configuration file path of the specified profile
+func getConfFilePath(profile string) string {
 	pwd, e := os.Getwd()
 	util.Panic(e)
 
-	yamlFile, e := ioutil.ReadFile(pwd + DEFAULT_CONF_PATH)
-	util.Panic(e)
+	file := pwd + DEFAULT_CONF_PATH
+	if profile != "" {
+		file += "-" + profile
+	}
+	return file
+}
 
-	c := new(conf)
-	e = yaml.Unmarshal(yamlFile, c)
-	util.Panic(e)
+func getStringProperty(con bool, v1 string, v2 string) string {
+	if con {
+		return v1
+	}
+	return v2
+}
 
-	return c
+func getIntProperty(con bool, v1 int, v2 int) int {
+	if con {
+		return v1
+	}
+	return v2
 }
