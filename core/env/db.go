7ebc4293c3f8a587d1f2dca71fc9f9ec15d4b8fc
duke.lu <duke.lu@ideamake.cn>
2021-02-05 16:31:45 +0800

    add multiple datasource support

--- core/env/db.go
+++ core/env/db.go
@@ -52,7 +52,7 @@ func InitDatabase(c *Conf) {
 			if connection, e := gorm.Open("mysql", dsn); connection != nil {
 				util.LogPanic(e)
 				connection.SingularTable(true)
-				mapper.DBS[name] = map[string]*gorm.DB{name: connection}
+				mapper.DBS[name] = map[string]*gorm.DB{database: connection}
 			}
 		}
 		log.Printf("Datasource complete initialization: %v - %v \n", name, getKeys(mapper.DBS[name]))
